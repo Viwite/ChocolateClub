5c9fdb59b77959ecc86f706ccf41cc36f6dce2fb
Oline <olinep@live.dk>
2022-06-03 13:20:11 +0200

    added logo billede

--- Includes/navigation.php
+++ Includes/navigation.php
@@ -1,6 +1,6 @@
 <nav class="navbar navbar-expand-lg bg-light">
     <div class="container-fluid">
-        <a class="navbar-brand" href="index.php"><img src="" alt="headerfoto" </a>
+        <a class="navbar-brand" href="index.php"><img src="images/kaninmorreveonkel.png" alt="logo foto" </a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
