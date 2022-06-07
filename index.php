7afd58d26f3901c78a0255f1d452f240401e5a1f
Oline <olinep@live.dk>
2022-06-07 12:38:29 +0200

    rettet til og added ny farve i bootstrap

--- index.php
+++ index.php
@@ -22,23 +22,26 @@
     <div class="carousel-inner">
         <div class="carousel-item active" data-bs-interval="10000">
             <img src="images/kaninmorreveonkel.png" class="d-block w-100" alt="chokolatiers">
-            <div class="carousel-caption d-none d-md-block">
-                <h5>First slide label</h5>
-                <p>Some representative placeholder content for the first slide.</p>
+            <div class="carousel-caption d-block">
+                <button type="button" class="btn btn-danger">
+                    <h3>Byg selv indpakning</h3>
+                </button>
             </div>
         </div>
-        <div class="carousel-item"> data-bs-interval="2000">
+        <div class="carousel-item" data-bs-interval="2000">
             <img src="images/væg.jpg" class="d-block w-100" alt="interiør">
-            <div class="carousel-caption d-none d-md-block">
-                <h5>Second slide label</h5>
-                <p>Some representative placeholder content for the second slide.</p>
+            <div class="carousel-caption d-block">
+                <button type="button" class="btn btn-danger">
+                    <h3>Byg selv indpakning</h3>
+                </button>
             </div>
         </div>
         <div class="carousel-item">
             <img src="images/smagsprøver%20hellyeah.jpg" class="d-block w-100" alt="smagsprøver">
-            <div class="carousel-caption d-none d-md-block">
-                <h5>Third slide label</h5>
-                <p>Some representative placeholder content for the third slide.</p>
+            <div class="carousel-caption d-block">
+                <button type="button" class="btn btn-danger">
+                    <h3>Byg selv indpakning</h3>
+                </button>
             </div>
         </div>
     </div>
@@ -54,11 +57,7 @@
 
 <br><br>
 
-<div class="row">
-    <div class="col-2">
-
-    </div>
-</div>
+<div class="container-fluid"></div>
 
 <?php include "includes/footer.php"; ?>
 <body>
