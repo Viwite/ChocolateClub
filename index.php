1eb3dc12068afd4990410a1b77c46ecbfd37f2b0
Oline <olinep@live.dk>
2022-06-14 11:54:32 +0200

    også rettet, så teksten har font-family, color og alignment (slettet ' og " fra koden)

--- index.php
+++ index.php
@@ -27,7 +27,7 @@
             <img src="images/kaninmorreveonkel.png" class="d-block w-100" alt="chokolatiers">
             <div class="carousel-caption d-block">
                 <button type="button" class="btn btn-danger">
-                    <h3>Byg selv</h3>
+                    <h3>Bestil</h3>
                 </button>
             </div>
         </div>
@@ -35,7 +35,7 @@
             <img src="images/væg.jpg" class="d-block w-100" alt="interiør">
             <div class="carousel-caption d-block">
                 <button type="button" class="btn btn-danger">
-                    <h3>Byg selv indpakning</h3>
+                    <h3>Bestil</h3>
                 </button>
             </div>
         </div>
@@ -43,7 +43,7 @@
             <img src="images/smagsprøver%20hellyeah.jpg" class="d-block w-100" alt="smagsprøver">
             <div class="carousel-caption d-block">
                 <button type="button" class="btn btn-danger">
-                    <h3>Byg selv indpakning</h3>
+                    <h3>Bestil</h3>
                 </button>
             </div>
         </div>
