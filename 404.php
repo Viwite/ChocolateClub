1c4d86ebe9a9297e1ac24f3556b9cc7bcd4d9545
Oline <olinep@live.dk>
2022-06-16 21:34:53 +0200

    ADDED ERROR PAGE

---
+++ 404.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+
+<html lang="da">
+
+<head>
+    <meta charset="utf-8">
+
+    <title>Sigende titel</title>
+
+    <meta name="robots" content="All">
+    <meta name="author" content="Udgiver">
+    <meta name="copyright" content="Information om copyright">
+
+    <link href="css/styles.css" rel="stylesheet" type="text/css">
+
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
+
+<body>
+
+<?php include "includes/navigation.php"; ?>
+
+<h1 class="text-1" style="text-align: center; font-family: Arial Rounded MT, sans-serif; color: #462f20;">ERROR</h1>
+<h1 class="text-1" style="text-align: center; font-family: Arial Rounded MT, sans-serif; color: #462f20;">404:</h1>
+<h1 class="text-1" style="text-align: center; font-family: Arial Rounded MT, sans-serif; color: #462f20;">CHOCOLATE</h1>
+<h1 class="text-1" style="text-align: center; font-family: Arial Rounded MT, sans-serif; color: #462f20;">WAS NOT</h1>
+<h1 class="text-1" style="text-align: center; font-family: Arial Rounded MT, sans-serif; color: #462f20;">FOUND!!</h1>
+
+<?php include "includes/footer.php"; ?>
+
+</body>
+</html>
