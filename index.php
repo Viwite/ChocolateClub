b65b14f2c704684a2d720975d4929b4de30809a6
Oline <olinep@live.dk>
2022-06-16 20:10:55 +0200

    leger

--- index.php
+++ index.php
@@ -22,6 +22,46 @@
 
 <?php include "includes/navigation.php"; ?>
 
+<nav class="navbar navbar-expand-lg bg-cremehvid">
+    <div class="container-fluid">
+        <div class="navbar-brand d-flex justify-content-between w-100"><a href="index.php">
+
+                <img src="images/logo2.png" class="d-lg-none" style="max-width: 150px;"
+                     alt="logo foto"></a>
+
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                    data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+                    aria-expanded="false" aria-label="Toggle navigation">
+                <i class="fa-solid fa-bars"></i>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li class="nav-item d-none d-lg-block" style="max-width: 150px;">
+                        <a href="index.php"><img src="images/logo2.png"
+                                                 alt="logo foto"></a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="BygSelv.php"><h4 class="nav-link" style="color: #462f20">Vælg Selv</h4></a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="voreschokolade.php"><h4 class="nav-link" style="color: #462f20">Vores Chokolade</h4></a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="omos.php"><h4 class="nav-link" style="color: #462f20">Om Os</h4></a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="contact.php"><h4 class="nav-link" style="color: #462f20">Kontakt</h4></a>
+                    </li>
+                </ul>
+                <form class="d-flex" role="search">
+                    <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+                    <button class="btn btn-outline-success" type="submit">Search</button>
+                </form>
+            </div>
+        </div>
+    </div>
+</nav>
+
 <main id="carouselExampleDark" class="carousel carousel-dark slide" data-bs-ride="carousel">
     <div class="carousel-inner">
         <div class="carousel-item active" data-bs-interval="10000">
