4eba15f64aa29258c64907a03bfe121c2c65cc3c
Oline <olinep@live.dk>
2022-06-16 21:42:08 +0200

    linket 404 side til byg selv

--- 404.php
+++ 404.php
@@ -1,17 +1,18 @@
 <!DOCTYPE html>
-
 <html lang="da">
-
 <head>
     <meta charset="utf-8">
 
-    <title>Sigende titel</title>
+    <title>Chocolate Club</title>
 
-    <meta name="robots" content="All">
+    <meta name=”description" content=”Chokolade, salg">
+    <meta name="robots" content="noindex, nofollow, all">
     <meta name="author" content="Udgiver">
     <meta name="copyright" content="Information om copyright">
 
+    <link href="css/bootstrap.css" rel="stylesheet" type="text/css">
     <link href="css/styles.css" rel="stylesheet" type="text/css">
+    <script src="https://kit.fontawesome.com/b2cd0c4e29.js" crossorigin="anonymous"></script>
 
     <meta name="viewport" content="width=device-width, initial-scale=1">
 </head>
@@ -20,11 +21,21 @@
 
 <?php include "includes/navigation.php"; ?>
 
-<h1 class="text-1" style="text-align: center; font-family: Arial Rounded MT, sans-serif; color: #462f20;">ERROR</h1>
-<h1 class="text-1" style="text-align: center; font-family: Arial Rounded MT, sans-serif; color: #462f20;">404:</h1>
-<h1 class="text-1" style="text-align: center; font-family: Arial Rounded MT, sans-serif; color: #462f20;">CHOCOLATE</h1>
-<h1 class="text-1" style="text-align: center; font-family: Arial Rounded MT, sans-serif; color: #462f20;">WAS NOT</h1>
-<h1 class="text-1" style="text-align: center; font-family: Arial Rounded MT, sans-serif; color: #462f20;">FOUND!!</h1>
+
+<div class="container-fluid p-5">
+    <h1 class="text-1" style="text-align: center; font-family: Arial Rounded MT, sans-serif; color: #462f20;">ERROR</h1>
+    <h1 class="text-1" style="text-align: center; font-family: Arial Rounded MT, sans-serif; color: #462f20;">404:</h1>
+    <h1 class="text-1" style="text-align: center; font-family: Arial Rounded MT, sans-serif; color: #462f20;">
+        CHOCOLATE</h1>
+    <h1 class="text-1" style="text-align: center; font-family: Arial Rounded MT, sans-serif; color: #462f20;">WAS
+        NOT</h1>
+    <h1 class="text-1" style="text-align: center; font-family: Arial Rounded MT, sans-serif; color: #462f20;">FOUND!!</h1>
+    <h3 class="text-1" style="text-align: center; font-family: Arial Rounded MT, sans-serif; color: #462f20;">thats so sad :'((</h3>
+
+    <h3 class="text-1" style="text-align: center; font-family: Arial Rounded MT, sans-serif; color: #462f20;">please go back to the main page</h3>
+
+</div>
+
 
 <?php include "includes/footer.php"; ?>
 
